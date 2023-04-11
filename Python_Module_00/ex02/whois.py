8b4a9722ab7805e88980dc8f26f923f1a1cee5e8
Ismail Mannoucha <ismannou@c2r3s5.42malaga.com>
2023-04-11 17:13:40 +0200

    even odd zero..

---
+++ Python_Module_00/ex02/whois.py
@@ -0,0 +1,18 @@
+import sys
+
+if (len(sys.argv) != 2):
+    print("You must provide ONE integer as an argument.")
+    exit()
+
+try:
+    assert sys.argv[1].isnumeric()
+except AssertionError:
+    print('AssertionError: argument is not an integer')
+    exit()
+
+if (sys.argv[1] == '0'):
+    print("I'm Zero.")
+elif (int(sys.argv[1]) % 2 == 0):
+    print("I'm Even.")
+else:
+    print("I'm Odd.")
