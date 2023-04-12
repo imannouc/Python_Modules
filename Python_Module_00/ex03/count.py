8716dc744e2257d7b020df7f8856225e5ee8a928
Ismail Mannoucha <ismannou@c2r3s5.42malaga.com>
2023-04-12 15:28:04 +0200

    ex03 done

--- Python_Module_00/ex03/count.py
+++ Python_Module_00/ex03/count.py
@@ -1,14 +1,16 @@
-import string
+import string, sys
 
 def text_analyzer(str=None):
     '''This function counts the number of upper characters, lower characters, punctuation and spaces in a given text.'''
     if (str == None):
-        str = input()
+        str = input('What is the text to analyze?\n')
+    try:
+        assert type(str).__name__ == 'str'
+    except AssertionError:
+        print('AssertionError: argument is not a string')
+        return
     print("The text contains",len(str),"character(s):")
-    lowerCount = 0
-    upperCount = 0
-    punctCount = 0
-    spaceCount = 0
+    lowerCount, upperCount, punctCount, spaceCount = 0,0,0,0
     for c in str:
         if (c in string.punctuation):
             punctCount += 1
@@ -23,8 +25,16 @@ def text_analyzer(str=None):
     print("-",punctCount,"punctuation mark(s)")
     print("-",spaceCount,"space(s)")
 
-def main():
-    text_analyzer("Python 2.0, released 2000, introduced features like List comprehensions and a garbage collection system capable of collecting reference cycles.")
-    text_analyzer("Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.")
-    text_analyzer()
-main()
+if __name__ == '__main__':
+    try:
+        assert len(sys.argv) == 2
+    except AssertionError:
+        print('AssertionError: You MUST provide one argument (only).')
+        exit()
+    text_analyzer(sys.argv[1])
+
+# def main():
+#     text_analyzer("Python 2.0, released 2000, introduced features like List comprehensions and a garbage collection system capable of collecting reference cycles.")
+#     text_analyzer("Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.")
+#     text_analyzer()
+# main()
