6416bf3b09edb10bd595251c1c6847493e5fbc27
Ismail Mannoucha <ismannou@c2r3s5.42malaga.com>
2023-04-14 21:03:32 +0200

    ex08 in progress

---
+++ Python_Module_00/ex08/sos.py
@@ -0,0 +1,29 @@
+import sys
+morse_code = {
+    'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ' ':'/'
+}
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        print('ERROR: arguments required.')
+        exit()
+    sys.argv.pop(0)
+    str = ' '.join(sys.argv)
+    if not all(c.isalnum() or c == ' ' for c in str):
+        print('ERROR: Enter a string containing alphanumeric characters and/or space only.')
+        exit()
+    print(str)
+    s = map(lambda x : morse_code[x], str)
+    print(list(s))
