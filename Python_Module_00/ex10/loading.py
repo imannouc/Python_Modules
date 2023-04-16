6e73da08d23bde8d01d139c47f4529d3cb3299c4
Ismail Mannoucha <ismannou@c2r3s5.42malaga.com>
2023-04-16 11:52:53 +0200

    eta , elapsed time , percent ..

--- Python_Module_00/ex10/loading.py
+++ Python_Module_00/ex10/loading.py
@@ -1,22 +1,34 @@
-from time import sleep
+from time import sleep, time
 
 
 # elapsed seconds,
 # 2.33 / 233 * (1000-233)
-# 
 def ft_progress(lst):
-    current = 1
-    total = max(lst) + 1
-    print( current / total * 100)
-    # for current in lst:
-    #     print(f"ETA: {ETA}s [ {round(current / total * 100)}%][=====> ] {current}/{total} | elapsed time {(elapsed * 100)}s")
-    #     yield current
+    t0 = time()
+    elapsed = 0
+    total = len(lst)
+    if (total == 0):
+        print("Invalid range.")
+        exit()
+    eta = 10000
 
+    bar = '=>'
+    for i in lst:
+        yield i
+        tn = time()
+        elapsed = tn - t0
+        if ( i != 0 ):
+            eta = (elapsed / (i)) * (total - i)
+        if (round((i / total) * 100) % 50 == 0):
+            bar = '=' + bar
+        print(f"ETA: {round(eta,2)}s [ {round((i / total) * 100)}%] BAR {i + 1}/{total} | elapsed time {round(elapsed,2)}s",end='\r')
+# ETA: 14.67s [ 9%][=
+# ETA: 8.67s [ 23%][=
 listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
     ret += (elem + 3) % 5
-    sleep(0.01)
+    sleep(0.1)
 print()
 print(ret)
 
