9dd768282fd880350e1e6e4245cc241fae8cf65c
Ismail Mannoucha <ismannou@c2r3s2.42malaga.com>
2023-04-16 19:12:41 +0200

    00 finished, test and push :D

--- Python_Module_00/ex10/loading.py
+++ Python_Module_00/ex10/loading.py
@@ -1,6 +1,5 @@
 from time import sleep, time
 
-
 # elapsed seconds,
 # 2.33 / 233 * (1000-233)
 def ft_progress(lst):
@@ -10,36 +9,21 @@ def ft_progress(lst):
     if (total == 0):
         print("Invalid range.")
         exit()
-    eta = 10000
+    eta = 1
 
-    bar = '=>'
     for i in lst:
         yield i
         tn = time()
         elapsed = tn - t0
+        progressBar = f"[{'=' * round((i / total) * 100)}>{' ' *(100 - round((i / total) * 100))}]"
         if ( i != 0 ):
             eta = (elapsed / (i)) * (total - i)
-        if (round((i / total) * 100) % 50 == 0):
-            bar = '=' + bar
-        print(f"ETA: {round(eta,2)}s [ {round((i / total) * 100)}%] BAR {i + 1}/{total} | elapsed time {round(elapsed,2)}s",end='\r')
-# ETA: 14.67s [ 9%][=
-# ETA: 8.67s [ 23%][=
+        print(f"\rETA: {round(eta,2):>5.2f}s [{round((i / total) * 100):>3}%] {progressBar} {i + 1:>{len(str(total))}}/{total:>{len(str(total))}} | elapsed time {round(elapsed,2):>5.2f}s",end='')
+
 listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
     ret += (elem + 3) % 5
-    sleep(0.1)
+    sleep(0.01)
 print()
 print(ret)
-
-
-# EXAMPLE OF A GENERATOR FUNCTION
-
-# def numeros_naturales(t):
-#   n = 1
-#   for n in t:
-#     yield n
-#     # n += 1
- 
-# for natural in numeros_naturales(range(100)):
-#    print(natural)
